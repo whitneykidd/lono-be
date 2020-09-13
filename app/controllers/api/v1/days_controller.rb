ef22c5302758f376bb2bc1e3d42eeb5288ace014
Whitney Kidd <whitneymkidd@gmail.com>
2020-09-13 13:33:25 -0600

    add days#create and route

--- app/controllers/api/v1/days_controller.rb
+++ app/controllers/api/v1/days_controller.rb
@@ -2,4 +2,14 @@ class Api::V1::DaysController < ApplicationController
   def index
     render json: Day.all
   end
+
+  def create
+    render json: Day.create(day_args)
+  end
+
+  private
+
+  def day_args
+    params.require(:day).permit(:temperature, :date)
+  end
 end
--- spec/requests/api/v1/days_request_spec.rb
+++ spec/requests/api/v1/days_request_spec.rb
@@ -8,6 +8,19 @@ describe "Days API" do
 
     expect(response).to be_successful
     days = JSON.parse(response.body)
+    # binding.pry
     expect(days.count).to eq(3)
+  end
+
+  it "can create a new day" do
+    day_args = { temperature: 97.1, date:"2020-09-09" }
+    headers = { "CONTENT_TYPE" => "application/json"}
+
+    post '/api/v1/days', headers: headers, params: JSON.generate({ day: day_args})
+    day = Day.last
+# binding.pry
+    expect(response).to be_successful
+    expect(day.temperature).to eq(day_args[:temperature])
+    expect(day.date).to eq(day_args[:date])
   end
 end
