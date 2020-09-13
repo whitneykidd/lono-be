cd634dc801a4d7b71061f1040c2848b49402e173
Whitney Kidd <whitneymkidd@gmail.com>
2020-09-13 14:34:42 -0600

    update user_data controller

--- app/controllers/api/v1/user_data_controller.rb
+++ app/controllers/api/v1/user_data_controller.rb
@@ -2,4 +2,14 @@ class Api::V1::UserDataController < ApplicationController
   def index
     render json: UserData.all
   end
+
+  def create
+    render json: UserData.create(data_args)
+  end
+
+  private
+
+  def data_args
+    params.permit(:start_date, :avg_length, :avg_cycle)
+  end
 end
--- spec/requests/api/v1/user_data_request_spec.rb
+++ spec/requests/api/v1/user_data_request_spec.rb
@@ -8,6 +8,19 @@ describe "User Data API" do
 
     expect(response).to be_successful
     user_data = JSON.parse(response.body)
-    binding.pry
+    expect(user_data.count).to eq(3)
+  end
+
+  it "can create a new user_data" do
+    data_args = {start_date: "2014-02-24", avg_length: 7, avg_cycle: 27 }
+    headers = { "CONTENT_TYPE" => "application/json" }
+
+    post "/api/v1/user_data", headers: headers, params: JSON.generate(user_data: data_args)
+    user_data = UserData.last
+
+    expect(response).to be_successful
+    expect(user_data.start_date).to eq("2014-02-24")
+    expect(user_data.avg_length).to eq(7)
+    expect(user_data.avg_cycle).to eq(27)
   end
 end
