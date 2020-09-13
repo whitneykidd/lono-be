98911ca6400c5f9f12e4b97a9ee604bb25923abb
Whitney Kidd <whitneymkidd@gmail.com>
2020-09-13 16:32:07 -0600

    update cors connection

--- config/application.rb
+++ config/application.rb
@@ -30,14 +30,10 @@ module LonoBe
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+  config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: [:get, :patch, :put, :delete, :post, :options]
-          )
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
   end
