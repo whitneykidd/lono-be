68006df5c96de42ae045e3a39fa83bd1e10cb74e
Mad Hallé <mad.halle@icloud.com>
2020-09-14 18:38:34 -0600

    user post fix

--- app/models/day.rb
+++ app/models/day.rb
@@ -5,16 +5,18 @@ class Day < ApplicationRecord
     temperature = day_info["temperature"]
     date = day_info["date"]
     high_risk = false
+    user_id = ["user_id"]
     # ^ could send to helper method that determines risk
-    daily_info = prep_day_info(temperature, date, high_risk)
+    daily_info = prep_day_info(temperature, date, high_risk, user_id)
     new(daily_info)
   end
 
-  def self.prep_day_info(temp, date, risk)
+  def self.prep_day_info(temp, date, risk, user_id)
     {
       temperature: temp,
       date: date,
-      high_risk: risk
+      high_risk: risk,
+      user_id: user_id
     }
   end
 end
