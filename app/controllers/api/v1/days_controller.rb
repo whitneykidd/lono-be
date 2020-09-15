ceb274d34f56ad1cb3904623f81e6e9537cd4363
Mad Hallé <mad.halle@icloud.com>
2020-09-14 20:19:41 -0600

    day should not be posting to db

--- app/controllers/api/v1/days_controller.rb
+++ app/controllers/api/v1/days_controller.rb
@@ -1,15 +1,19 @@
 class Api::V1::DaysController < ApplicationController
   def index
     render json: Day.all
-    # binding.pry
   end
 
   def create
     day = Day.new_day(day_args)
-    user1 = User.find_by(name: day_args["name"])
-    day_data = day_args.except("name")
-    total_day_data = day_data.merge!(user_id: user1.id)
-    render json: Day.create(total_day_data)
+    day_saved = day.save
+    # user1 = User.find_by(name: day_args["name"])
+    # require "pry"; binding.pry
+    # day_data = day_args.except("name")
+    require "pry"; binding.pry
+    # total_day_data = day_data.merge!(user_id: user1.id)
+    # require "pry"; binding.pry
+    # render json: Day.create(total_day_data)
+    render json: day_saved
   end
 
   private
